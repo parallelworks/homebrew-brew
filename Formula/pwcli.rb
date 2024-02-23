36b3f9fa2dc181fc8dc86b353e425fbb3c752562
Grey Singh <aps152@georgetown.edu>
2024-02-22 23:40:26 -0600

    trying to get this working

--- Formula/pwcli.rb
+++ Formula/pwcli.rb
@@ -1,7 +1,7 @@
 require "formula"
 require_relative "../auth_dl.rb"
 
-class Wget < Formula
+class Pwcli < Formula
   homepage "https://github.com/parallelworks/homebrew-brew/tree/master"
   url "https://github.com/parallelworks/core/releases/download/v4.10.2/pwcli-darwin-arm64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "2505b477d0aa0004a346d8fe043a4e2a07db0d16e48a55ca249fdaeca420974a"
@@ -12,6 +12,6 @@ class Wget < Formula
   end
 
   test do
-      system "#{bin}/wget --help"
+      system "#{bin}/pw --help"
   end
 end

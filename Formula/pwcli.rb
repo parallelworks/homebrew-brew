6baed53c8007f7400a0660bb4bd1b37921d4eae7
Grey Singh <aps152@georgetown.edu>
2024-02-23 02:20:56 -0600

    Updated README

--- Formula/pwcli.rb
+++ Formula/pwcli.rb
@@ -5,7 +5,7 @@ class Pwcli < Formula
   desc "Parallel Works CLI"
   homepage "https://github.com/parallelworks/homebrew-brew/tree/master"
 
-  #if Hardware::CPU.intel?
+  #What if Hardware::CPU.intel?
   url "https://github.com/parallelworks/core/releases/download/v4.10.2/pwcli-darwin-arm64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "2505b477d0aa0004a346d8fe043a4e2a07db0d16e48a55ca249fdaeca420974a"
 
--- README.md
+++ README.md
@@ -10,3 +10,11 @@ You can add the custom tap on MacOS by running:
 ```shell
 brew tap parallelworks/brew
 ```
+
+Then install the cli by running:
+
+```shell
+brew install pwcli
+```
+
+Note: must have environment variable HOMEBREW_GITHUB_API_TOKEN with a token that has access to private repos.

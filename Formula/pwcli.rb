bdd74a512da55fe9fa849153c0751c02e383c1b9
Grey Singh <aps152@georgetown.edu>
2024-02-23 01:52:20 -0600

    fixed maybe

--- Formula/pwcli.rb
+++ Formula/pwcli.rb
@@ -2,18 +2,18 @@ require "formula"
 require_relative "../auth_dl.rb"
 
 class Pwcli < Formula
+  desc "Parallel Works CLI"
   homepage "https://github.com/parallelworks/homebrew-brew/tree/master"
+
+  #if Hardware::CPU.intel?
   url "https://github.com/parallelworks/core/releases/download/v4.10.2/pwcli-darwin-arm64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "2505b477d0aa0004a346d8fe043a4e2a07db0d16e48a55ca249fdaeca420974a"
 
   def install
-    system "mv", "pwcli-darwin-arm64", "#{bin}/pw"
-    system "chmod", "+x", "#{bin}/pw" 
-    #system "./configure", "--prefix=#{prefix}"
-    #system "make", "install"
+    bin.install "pw"
   end
 
   test do
-      system "#{bin}/pw --help"
+    system "#{bin}/pw --version"
   end
 end
